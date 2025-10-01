4b309065ad9abce0021253f7cb3d5d1b30262fee
Nathan Buche <nathan.buche@theodo.com>
2025-10-01 14:05:12 +0200

    state & fetch

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,15 +1,19 @@
 interface Props {
   name: string
   id: number
+  height: number
+  weight: number
 }
 
-export const Pokemon = ({ name, id }: Props) => {
+export const Pokemon = ({ name, id, height, weight }: Props) => {
   console.log("Rendering Pokemon:", name)
   return (
     <div>
       <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={name} />
       <p>Name: {name}</p>
       <p>Number: {id}</p>
+      <p>Height: {height}</p>
+      <p>Weight: {weight}</p>
     </div>
   )
 }
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,18 +1,48 @@
 import styles from "./Home.module.css"
 import { Pokemon } from "../../components/Pokemon"
+import React from "react"
 
-const pokemonList = [
-  { id: 7, name: "Carapuce" },
-  { id: 8, name: "Carabaffe" },
-  { id: 9, name: "Tortank" },
-]
+interface PokemonInfo {
+  id: number
+  name: string
+  height: number
+  weight: number
+}
+
+function filterPokemonByName(pokeList: PokemonInfo[], filterValue: string) {
+  return pokeList.filter(pokemon => pokemon.name.toLowerCase().includes(filterValue.toLowerCase()))
+}
+
+function fetchPokemons() {
+  return fetch("http://localhost:8000/pokemons", { headers: { accept: "application/json" } }).then(response =>
+    response.json(),
+  )
+}
 
 export const Home = () => {
+  const [pokemonFilterValue, setFilterValue] = React.useState("")
+  const [pokemonList, updatePokemonList] = React.useState<PokemonInfo[]>([])
+
+  React.useEffect(() => {
+    fetchPokemons().then(data => {
+      updatePokemonList(data)
+    })
+  }, [])
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterValue(event.target.value)
+  }
   return (
     <div className={styles.intro}>
       <div>Pokédex</div>
-      {pokemonList.map(({ name, id }) => (
-        <Pokemon key={id} id={id} name={name} />
+      <input
+        className={styles.input}
+        onChange={onInputChange}
+        value={pokemonFilterValue}
+        placeholder="Cherchez un pokémon"
+      />
+      {filterPokemonByName(pokemonList, pokemonFilterValue).map(({ name, id, height, weight }) => (
+        <Pokemon key={id} id={id} name={name} height={height} weight={weight} />
       ))}
     </div>
   )
