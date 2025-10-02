7957c6939f4830ca09041cd1f02458e5d33dde0d
Nathan Buche <nathan.buche@theodo.com>
2025-10-02 15:52:19 +0200

    Ajout route / redirection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { Root } from "./components/Root"
 
 import { Home } from "./pages/Home"
 import { Pokemon } from "./pages/Pokemon"
+import { Pokedex } from "./pages/Pokedex"
 
 export const App = () => {
   return (
     <Root>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Routes>
+          <Route path="/" element={<Navigate to="/pokedex/0" replace />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/pokemon/:id" element={<Pokemon />} />
+          <Route path="/pokedex/:id" element={<Pokedex />} />
         </Routes>
       </BrowserRouter>
     </Root>
