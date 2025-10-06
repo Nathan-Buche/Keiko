7bcec536684ae60f2d78a8e89e4b1913e2d81649
Nathan Buche <nathan.buche@theodo.com>
2025-10-06 10:33:13 +0200

    Add HOC animate

--- frontend/src/components/Animate/Animate.tsx
+++ frontend/src/components/Animate/Animate.tsx
@@ -1,1 +1,28 @@
-export const Animate = () => <></>
+import React from "react"
+import styles from "./Animate.module.css"
+
+export const Animate = <P extends object>(BaseComponent: React.ComponentType<P>) => {
+  const WrappedComponent = (props: P) => {
+    const [isAnimating, setIsAnimating] = React.useState(false)
+
+    return (
+      <div
+        id="AnimatedComponent"
+        className={`${styles.wrapper} ${isAnimating ? styles.tadaAnimation : ""}`}
+        onMouseEnter={() => {
+          console.log("mouseEnter")
+          setIsAnimating(true)
+        }}
+        onMouseLeave={() => {
+          console.log("mouseLeave")
+          setIsAnimating(false)
+        }}
+      >
+        <BaseComponent {...props} />
+      </div>
+    )
+  }
+
+  WrappedComponent.displayName = `Animate(${BaseComponent.displayName || BaseComponent.name || "Component"})`
+  return WrappedComponent
+}
